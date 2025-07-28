81a3cd3460a14db874d3ef5a1d26614b7b67ca8d
ManyToMany <sahar.zs2002@gmail.com>
2025-07-28 20:06:27 +0330

    1

--- script.js
+++ script.js
@@ -1,49 +1,55 @@
 'use strict';
 
-// Weather App
 const input = document.querySelector('.input');
 const btnSearch = document.querySelector('.btn__search');
 const article = document.querySelector('article');
 const apiKey = '3366667e3901a854740f2c0b55f891cc';
 
+// Weather App
 btnSearch.addEventListener('click', function (e) {
   e.preventDefault();
-
   if (input.value === '') alert('fill the inpute!');
   else {
-    // console.log(input.value);
-    const html = `
+    // fetch API
+    const fetchData = function (location) {
+      fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${apiKey}`
+      )
+        .then(response => response.json())
+        .then(data => {
+          console.log(data);
+          renderData(data);
+        });
+    };
+    fetchData(input.value);
+
+    // render data
+    const renderData = function (data) {
+      const html = `
     <article>
+        <div class="sky__img">
+          <img src="sky-images/02d@2x.png" />
+        </div>
+
         <div>
-          <h4 class="city__temperature">27o C</h4>
-          <h3 class="country__name">Iran</h3>
-          <h4 class="city__name">
-            Tehran <span class="city__calender__hour">2025-07-27 16:38</span>
-          </h4>
+          <h4 class="city__temperature">${Math.round(data.main.temp)}&deg;C</h4>
+          <h3 class="location">${data.name}, ${data.sys.country} </h3>
+          <!-- <h4 class="city__calender__hour">2025-07-27 16:38</h4> -->
           <div class="climator__flex">
-            <p class="sky__like">light rain shower</p>
+            <p class="sky__like">${data.weather[0].description}</p>
             <div>
-              <p class="humidity">Humidity: 90%</p>
-              <p class="wind">Wind: 13.7 km/h</p>
-            </div>
+              <p class="humidity">Humidity: ${data.main.humidity}%</p>
+              <p class="wind">Wind: ${data.wind.speed} km/h</p>
+          </div>
           </div>
         </div>
-        <div class="sky__img">
-          <img src="images/mobile.jpg" />
-        </div>
+        
       </article>
     `;
-
-    article.insertAdjacentHTML('afterbegin', html);
-
-    // fetch API
-    // const weatherSearch = function (cityName) {
-    //   fetch(
-    //     `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`
-    //   )
-    //     .then(response => response.json())
-    //     .then(data => console.log(data));
-    // };
-    // weatherSearch('tehran');
+      article.insertAdjacentHTML('afterbegin', html);
+    };
   }
 });
+
+const dt = 2759794;
+const day = new Date(dt * 1000);
